4b4fe5ca721815d02868aadc909fad6403c467b8
lexx mg <lexxmg@iMac-lexx-3.local>
2020-09-04 10:31:47 +0300

    add listener

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,49 @@
-$.datepicker.regional['ru'] = {
-	closeText: 'Закрыть',
-	prevText: 'Предыдущий',
-	nextText: 'Следующий',
-	currentText: 'Сегодня',
-	monthNames: ['Январь','Февраль','Март','Апрель','Май','Июнь','Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
-	monthNamesShort: ['Янв','Фев','Мар','Апр','Май','Июн','Июл','Авг','Сен','Окт','Ноя','Дек'],
-	dayNames: ['воскресенье','понедельник','вторник','среда','четверг','пятница','суббота'],
-	dayNamesShort: ['вск','пнд','втр','срд','чтв','птн','сбт'],
-	dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
-	weekHeader: 'Не',
-	dateFormat: 'dd.mm.yy',
-	firstDay: 1,
-	isRTL: false,
-	showMonthAfterYear: false,
-  changeMonth: true,
-  changeYear: true,
-	yearSuffix: ''
-};
-$.datepicker.setDefaults($.datepicker.regional['ru']);
-
 $(function(){
+  const form = $('.form');
+
+  form.on('focusout', event => {
+    const target = event.target;
+    const valid = target.validity.valid;
+
+    if (!valid) {
+      target.value = '';
+    }
+  });
+
+  form.on('submit', event => {
+    event.preventDefault();
+
+    const data = form.serialize();
+    //console.log(this);
+    //console.log($.(this));
+
+    $.post('https://echo.htmlacademy.ru', data, res => {
+      alert(res);
+    });
+  });
+
+  $.datepicker.regional['ru'] = {
+  	closeText: 'Закрыть',
+  	prevText: 'Предыдущий',
+  	nextText: 'Следующий',
+  	currentText: 'Сегодня',
+  	monthNames: ['Январь','Февраль','Март','Апрель','Май','Июнь','Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
+  	monthNamesShort: ['Янв','Фев','Мар','Апр','Май','Июн','Июл','Авг','Сен','Окт','Ноя','Дек'],
+  	dayNames: ['воскресенье','понедельник','вторник','среда','четверг','пятница','суббота'],
+  	dayNamesShort: ['вск','пнд','втр','срд','чтв','птн','сбт'],
+  	dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
+  	weekHeader: 'Не',
+  	dateFormat: 'dd.mm.yy',
+  	firstDay: 1,
+  	isRTL: false,
+  	showMonthAfterYear: false,
+    changeMonth: true,
+    changeYear: true,
+  	yearSuffix: ''
+  };
+
+  $.datepicker.setDefaults($.datepicker.regional['ru']);
+
 	$('.form__input[name="date"]').datepicker();
 
   $.mask.definitions['~'] = "[78]";
