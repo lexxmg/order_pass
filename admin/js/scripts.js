8bc7e93e248b682a01cf92d53318221092744e7d
mac-book <lexx.mg@gmail.com>
2020-09-06 10:57:03 +0300

    finish line

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -8,19 +8,23 @@ $(function() {
     for (let obj of res) {
       let done;
 
-      if (obj.done) {
-        done = 'item__top--done';
+      if (obj.done === 'true') {
+        done = 'item--done';
       } else {
         done = '';
       }
 
       list.prepend(`
-      <li class="list__item item">
+      <li class="list__item item ${done}">
         <div class="item__container">
-          <div class="item__top ${done}">
+          <div class="item__top">
             <h2 class="item__title">${obj.firm}</h2>
 
-            <button class="item__btn-close">X</button>
+            <button class="item__btn-close btn-close"
+                    aria-label="открыть"
+                    aria-expanded="false"
+                    aria-controls="list-data">X
+            </button>
           </div>
 
           <div class="item__body item-body">
@@ -60,8 +64,8 @@ $(function() {
             </div>
 
             <div class="item-body__buttons buttons">
-              <button class="buttons__done">Сделано</button>
-              <button class="buttons__del">Удалить</button>
+              <button class="buttons__del btn btn--red">Удалить</button>
+              <button class="buttons__done btn">Сделано</button>
             </div>
           </div>
         </div>
@@ -73,30 +77,60 @@ $(function() {
   list.on('click', event => {
     const target = event.target;
 
-    if (event.target.className === 'buttons__del') {
+    if ( event.target.classList.contains('buttons__del') ) {
       let firm = target.closest('.item__container').querySelector('.item__title').textContent;
 
       if( confirm('Задача будет удалена') ) {
         $.get('http://192.168.0.15/order_pass/php/delete.php', {'firm': firm}, res => {
           console.log(res);
-        }).done( () => target.closest('.list__item').remove());
+        }).done( () => {
+          const el = target.closest('.list__item');
+          $(el).fadeOut(600, "linear", () => {
+            el.remove();
+          });
+        });
       }
     }
 
-    if (event.target.className === 'buttons__done') {
+    if ( event.target.classList.contains('buttons__done') ) {
       let firm = target.closest('.item__container').querySelector('.item__title').textContent;
+      let el = target.closest('.list__item');
 
-      $.get('http://192.168.0.15/order_pass/php/edit.php', {'firm': firm, 'done': true}, res => {
-        console.log(res);
+      $.getJSON('http://192.168.0.15/order_pass/php/read.php', res => {
+        for (let obj of res) {
+          if (obj.firm === firm) {
+            console.log(obj.firm);
+            console.log(obj.done);
+            if (obj.done === 'true') {
+              $.get('http://192.168.0.15/order_pass/php/edit.php', {'firm': firm, 'done': false}, res => {
+                console.log(res);
+              }).done(() => {
+                $(el).removeClass('item--done');
+              });
+            } else {
+              $.get('http://192.168.0.15/order_pass/php/edit.php', {'firm': firm, 'done': true}, res => {
+                console.log(res);
+              }).done(() => {
+                $(el).addClass('item--done');
+              });
+            }
+          }
+        }
       });
-      console.log(firm);
     }
 
     if ( event.target.classList.contains('item__btn-close') ) {
       let el = target.closest('.item__top').nextElementSibling;
 
-      $(el).slideToggle(1000);
-      console.log(el);
+      if ( $(target).attr('aria-expanded') === 'true') {
+        $(el).slideUp(400, () => {
+          $(target).attr('aria-expanded', 'false');
+        });
+      } else {
+        $(el).slideDown(400, () => {
+          $(target).attr('aria-expanded', 'true');
+        });
+      }
     }
   });
 });
