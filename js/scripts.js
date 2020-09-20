c3f489cce48723a10649a3107c39aa10680cafff
mac-book <lexx.mg@gmail.com>
2020-09-20 20:08:44 +0300

    finish

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,47 @@
 'use strict';
-/*
+
+const form = document.querySelector('.form'),
+      btn = document.querySelector('.form__button');
+
+form.addEventListener('focusout', event => {
+  const target = event.target;
+  const valid = target.validity.valid;
+
+  if (!valid) {
+    target.value = '';
+  }
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  btn.setAttribute('disabled', 'disabled');
+
+  let data = urlEncoded(form);
+  data += ('&key=passs');
+
+  post('data-rec.php', data, res => {
+    if (res === 'ok') {
+      popup('Заявка успешно получена', 3000, () => {
+        form.reset();
+        btn.removeAttribute('disabled');
+      });
+    } else if (res == 'err') {
+      popup('Ошибка сервера, повторите попытку', 3000, () => {
+        btn.removeAttribute('disabled');
+      });
+    }
+  }, stat => {
+    if (stat !== 200) {
+      popup('Сервер не отвечает', 3000, () => {
+        btn.removeAttribute('disabled');
+      });
+    }
+  });
+});
+
 try {
   $(function(){
-    const form = $('.form'),
-          btn = $('.form__button');
-
-
-    form.on('focusout', event => {
-      const target = event.target;
-      const valid = target.validity.valid;
-
-      if (!valid) {
-        target.value = '';
-      }
-    });
-
-    form.on('submit', event => {
-      event.preventDefault();
-
-      btn.attr('disabled', true);
-
-      const data = form.serializeArray();
-      data.push({name: 'key', value: 'passs'});
-
-      $.post('data-rec.php', data, res => {
-        if (res === 'ok') {
-          popup('Заявка успешно получена', 3000, () => {
-            form[0].reset();
-            btn.attr('disabled', false);
-          });
-        } else if (res == 'err') {
-          popup('Ошибка сервера, повторите попытку', 3000, () => {
-            btn.attr('disabled', false);
-          });
-        }
-      }).fail( () => {
-        popup('Сервер не отвечает', 3000, () => {
-          btn.attr('disabled', false);
-        });
-      });
-    });
 
     $.datepicker.regional['ru'] = {
     	closeText: 'Закрыть',
@@ -75,91 +77,85 @@ try {
     // }
     //$('.form__input[name="count"]').mask('99?');
   });
-
-  function popup(text, time, collback) {
-    scrollOff(true);
-    const div = document.createElement('div');
-
-    div.innerHTML = `
-      <div class="popup-container">
-        <div class="popup-container__inner">
-          <h3 class="popup-container__title">${text}</h3>
-
-          <button class="popup-container__btn btn-close-popup"
-                  aria-label="закрыть">
-          </button>
-        </div>
-      </div>
-    `;
-
-    div.addEventListener('click', (event) => {
-      const target = event.target;
-      if ( target.classList.contains('popup-container__btn') ) {
-        div.remove();
-        scrollOff(false);
-        collback();
-      }
-
-      if ( !target.closest('.popup-container__inner') ) {
-        div.remove();
-        scrollOff(false);
-        collback();
-      }
-    });
-
-   document.body.append(div);
-
-    if (time) {
-      setTimeout( () => {
-        div.remove();
-        scrollOff(false);
-        collback();
-      }, time);
-    }
-  }
-
-  function scrollOff(bul) {
-    const width = document.documentElement.clientWidth;
-    if (bul) {
-      document.documentElement.style.overflow = 'hidden';
-      if (width !== document.documentElement.clientWidth) {
-        const padding = document.documentElement.clientWidth - width;
-        document.body.style.paddingRight = padding + 'px';
-      }
-    } else {
-      document.documentElement.style.overflow = '';
-      document.body.style.paddingRight = '';
-    }
-  }
 } catch(e) {
   alert('В этом браузере сайт работает не корректно!!!' + ' ' + e);
 }
 
-*/
 
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++=
+function popup(text, time, collback) {
+  scrollOff(true);
+  const div = document.createElement('div');
 
-'use strict';
+  div.innerHTML = `
+    <div class="popup-container">
+      <div class="popup-container__inner">
+        <h3 class="popup-container__title">${text}</h3>
 
-const form = document.querySelector('.form');
+        <button class="popup-container__btn btn-close-popup"
+                aria-label="закрыть">
+        </button>
+      </div>
+    </div>
+  `;
 
-let formData = new FormData(form);
-//const data = {test: 'test'};
+  div.addEventListener('click', (event) => {
+    const target = event.target;
+    if ( target.classList.contains('popup-container__btn') ) {
+      div.remove();
+      scrollOff(false);
+      collback();
+    }
 
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
+    if ( !target.closest('.popup-container__inner') ) {
+      div.remove();
+      scrollOff(false);
+      collback();
+    }
+  });
 
-  let xhr = new XMLHttpRequest();
-  xhr.open('POST', 'http://192.168.0.15/echo.php');
+ document.body.append(div);
+
+  if (time) {
+    setTimeout( () => {
+      div.remove();
+      scrollOff(false);
+      collback();
+    }, time);
+  }
+}
+
+function scrollOff(bul) {
+  const width = document.documentElement.clientWidth;
+  if (bul) {
+    document.documentElement.style.overflow = 'hidden';
+    if (width !== document.documentElement.clientWidth) {
+      const padding = document.documentElement.clientWidth - width;
+      document.body.style.paddingRight = padding + 'px';
+    }
+  } else {
+    document.documentElement.style.overflow = '';
+    document.body.style.paddingRight = '';
+  }
+}
+
+function post(url, data, response, status) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('POST', url);
   xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 
   xhr.addEventListener('readystatechange', () => {
-    //if (xhr.redyState != 4) return;
-    console.log(xhr.redyState);
+    if (xhr.readyState != 4) return;
 
-    console.log(xhr.responseText);
+    status(xhr.status);
+
+    response(xhr.responseText);
   });
 
+  xhr.send(data);
+}
+
+function urlEncoded(form) {
   let arr = [];
 
   for (var i = 0; i < form.elements.length; i++) {
@@ -168,9 +164,5 @@ form.addEventListener('submit', (event) => {
     }
   }
 
-  arr.push('key'+ '=' + 'passs');
-
-  xhr.send(arr.join('&'));
-
-  console.log( arr.join('&') );
-});
+  return arr.join('&');
+}
