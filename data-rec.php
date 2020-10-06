2de579b455cf6b6b667ca1f5c1f93cf4fde63208
lexx mg <lexxmg@iMac-lexx-3.local>
2020-10-06 13:02:34 +0300

    edit data-rec.php

--- data-rec.php
+++ data-rec.php
@@ -1,4 +1,25 @@
 <?php
+require 'PHPMailer/PHPMailerAutoload.php';
+
+$mailAdmin = new PHPMailer;
+$mailUser = new PHPMailer;
+
+$mailAdmin->setFrom('propusk@stalco.ru', 'Mailer');
+$mailAdmin->addAddress('4996415641@mail.ru', 'User');
+//$mailAdmin->clearAddresses();
+//$mailAdminer->AddBCC('lexx.mg@yandex.ru'); //Добавляет адрес кому отправится скрытая копия
+//$mailAdmin->addAddress('lexx.mg@yandex.ru', 'User');
+
+$mailUser->isSMTP();
+$mailUser->CharSet = "UTF-8";
+//$mailAdmin->SMTPDebug = SMTP::DEBUG_SERVER;
+$mailUser->Host = 'smtp.gmail.com';
+$mailUser->SMTPAuth   = true;
+$mailUser->Username   = '@gmail.com';
+$mailUser->Password   = 's';
+$mailUser->Port = 587;
+$mailUser->SMTPSecure = 'tls';
+$mailUser->setFrom("propusk.stalco@gmail.com", "ПРОПУСК"); // От кого отправлять
 
 //======================================= пишем GET запросы в лог ==================================
 
@@ -37,6 +58,51 @@ if ($_POST['key'] == 'passs') {
   };
 
   $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+  //____________Отправка почты_________________________________________________
+    try {
+      $mailAdmin->CharSet = "UTF-8";
+      //$mailAdmin->SMTPDebug = SMTP::DEBUG_SERVER;
+      //$mailAdmin->isSMTP();
+      $mailAdmin->isHTML(true);//HTML формат
+      $mailAdmin->Subject = "Новый пропуск";
+      $mailAdmin->Body    = "Название компании: {$_POST['firm']}<br>
+                             Колличество пропусков: {$_POST['count']}<br><br>
+                             <b>Кнтакты:</b><br>
+                             ФИО: {$_POST['fio']}<br>
+                             Должность: {$_POST['post']}<br>
+                             Email: {$_POST['email']}<br>
+                             Телефон: {$_POST['tel']}";
+      $mailAdmin->AltBody = "Альтернативное содержание сообщения";
+      $mailAdmin->send();
+      //echo "Сообщение отправлено";
+
+
+    } catch (Exception $e) {
+      //echo "Ошибка отправки: {$mailAdmin->ErrorInfo}";
+    }
+
+    try {
+          $mailUser->addAddress($_POST['email'], $_POST['fio']);
+          $mailUser->isHTML(true);//HTML формат
+          $mailUser->Subject = "Заявка получена";
+          $mailUser->Body    = "Уважаемый арендатор!<br><br>
+                                 Ваша заявка на электронные карты-пропуска в количестве {$_POST['count']} шт. принята.<br>
+                                 Информация о готовности пропусков поступит на Ваш адрес электронной почты.<br><br>
+                                 <b>По возникшим вопросам обращайтесь:</b><br>
+                                 Тел:<a style='margin-left: 5px; text-decoration: none' href='tel:84996415641'>+7(499) 641-5-641</a>,
+                                 		 <a style='margin-left: 5px; text-decoration: none' href='tel:84996415601'>+7(499) 641-56-01</a><br>
+                                 e-mail:<a style='margin-left: 5px; text-decoration: none' href='mailto:propusk.stalco@gmail.com'>propusk.stalco@gmail.com</a>";
+
+          $mailUser->AltBody = "Ваша заявка на электронные карты-пропуска в количестве {$_POST['count']} шт. принята."; //Альтернативное сообщение
+          $mailUser->send();
+
+          //echo "Сообщение отправлено";
+        } catch (Exception $e) {
+          //echo "Ошибка отправки: {$mailAdmin->ErrorInfo}";
+        }
+  //____________________________________________________________________________
+
 
 } else {
   echo "err";
